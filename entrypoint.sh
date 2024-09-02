39b35b38aec125652f0bfcbd2081c80d02416b81
HUNG NGO <hungngo1607@gmail.com>
2024-09-02 15:06:51 -0400

    add docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-# Start the Celery worker
-celery -A tasks.app worker --loglevel=info &
-
-sleep 15
-# Run the Python consumer script
-echo "Listing files:" && ls -la
-
-python run_consumer.py
+celery -A tasks.app purge -f
+celery -A tasks.app worker --loglevel=info 
 
 # Wait for all background jobs to finish
 wait
